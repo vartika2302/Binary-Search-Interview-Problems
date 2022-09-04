1eef7e7c214393092e18ae24900b26752203c676
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-09-04 16:37:43 +0530

    Time: 12 ms (33.66%), Space: 11.6 MB (61.36%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+    int firstPos(vector<int>& nums,int target,int n){
+        int s=0;
+        int e=n-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=s+((e-s)/2);
+            if(nums[mid]==target){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+    
+    int lastPos(vector<int>& nums, int target, int n){
+        int s=0;
+        int e=n-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=s+((e-s)/2);
+            if(nums[mid]==target){
+                ans=mid;
+                s=mid+1;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+    
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>res;
+        int n = nums.size();
+        int s=firstPos(nums,target,n);
+        if(s==-1) return res;
+        int e=lastPos(nums,target,n);
+        for(int k=s;k<=e;k++){
+            res.push_back(k);
+        }
+        return res;
+    }
+};
